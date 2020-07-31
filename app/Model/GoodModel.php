b887574d7fc93c65591270a2d738f0ac51e16724
xie <xie@localhost.localdomain>
2020-07-31 02:49:19 +0000

    添加

---
+++ app/Model/GoodModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoodModel extends Model
+{
+    //
+    public $table ='p_goods';
+    public $primaryKey='goods_id';
+    public $timestamps=false;
+}
